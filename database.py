653a15fd5f2a3486cb20034979d82f5b7f9bb732
Aranda Paige <ArandaPaige@gmail.com>
2021-05-10 01:30:04 -0500

    Start of DB Manager class.

--- database.py
+++ database.py
@@ -1,4 +1,31 @@
+import logging
+import classes
+import pg8000
+
+from dotenv import dotenv_values
+
 from sqlalchemy import text, create_engine
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+logger = logging.getLogger(__name__)
 
+USER, KEY_VAL, HOST, DB = (env for env in dotenv_values('.env').values())
+
+#ENGINE = create_engine(f"postgresql+pg8000://{USER}:{KEY_VAL}@{HOST}/{DB}", client_encoding='utf8')
+
+
+class DatabaseManager:
+    """Define"""
+
+    def __init__(self, engine, db_base):
+        """Define"""
+        self.engine = engine
+        self.db_base = db_base
+
+    def __str__(self):
+        return f'{self.engine}, {self.db_base}'
+
+    def __repr__(self):
+        return f'{__class__.__name__}({self.engine}, {self.db_base})'
+
+    def initialize_tables(self):
+        self.db_base.metadata.create_all(self.engine)
