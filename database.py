ebf18e78d55c96ee50a94260377b8596d636073a
Aranda Paige <ArandaPaige@gmail.com>
2021-05-11 13:39:16 -0500

    Removed DB manager class. Created session context manager decorator.

--- database.py
+++ database.py
@@ -13,45 +13,44 @@ USER, KEY_VAL, HOST, DB = (env for env in dotenv_values('.env').values())
 
 #ENGINE = create_engine(f"postgresql+pg8000://{USER}:{KEY_VAL}@{HOST}/{DB}", client_encoding='utf8')
 
+ENGINE = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+DB_BASE = classes.db_class
 
-class DatabaseManager:
-    """Define"""
 
-    def __init__(self, engine, db_base):
-        """Define"""
-        self.engine = engine
-        self.db_base = db_base
+def session_context_mgr(method):
+    session = Session(ENGINE)
 
-    def __str__(self):
-        return f'{self.engine}, {self.db_base}'
+    def session_wrapper(*args, **kwargs):
+        with session as sess:
+            result = method(sess, *args, **kwargs)
+            if result is not None:
+                return result
+            else:
+                sess.commit()
+                return
 
-    def __repr__(self):
-        return f'{__class__.__name__}({self.engine}, {self.db_base})'
+    return session_wrapper
 
-    def initialize_tables(self):
-        self.db_base.metadata.create_all(self.engine)
 
-    def session_context_mgr(self, method):
-        session = Session(self.engine)
+DB_BASE.metadata.create_all(ENGINE)
 
-        def session_wrapper(*args, **kwargs):
-            with session as sess, session.begin():
-                result = method(sess, *args, **kwargs)
-                if result is not None or len(result) > 0:
-                    return result
-                else:
-                    sess.commit()
-                    return
-        return session_wrapper
 
-    @session_context_mgr
-    def add_instance(self, session, *instances):
-        for instance in instances:
-            session.add(instance)
-        return
+@session_context_mgr
+def add_instance(session, instances):
+    for instance in instances:
+        session.add(instance)
+    return
 
-    @session_context_mgr
-    def delete_obj(self, session, *obj):
-        for obj in obj:
-            session.delete(obj)
-        return
+
+@session_context_mgr
+def delete_obj(session, objects):
+    for obj in objects:
+        session.delete(obj)
+    return
+
+
+DB_BASE.metadata.create_all(ENGINE)
+user = classes.User('BoogtehWoog', 'BoogtehWoog@gmail.com', 'Boogest', 185, 180)
+
+
+add_instance((user,))
