70ee3ea90d610d0e513c3aa481542c4cca437949
Aranda Paige <ArandaPaige@gmail.com>
2021-05-11 22:53:18 -0500

    Work continues on DB functions. Rough implementation of select added.

--- database.py
+++ database.py
@@ -4,17 +4,18 @@ import pg8000
 
 from dotenv import dotenv_values
 
-from sqlalchemy import text, create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 
 logger = logging.getLogger(__name__)
 
 USER, KEY_VAL, HOST, DB = (env for env in dotenv_values('.env').values())
 
-#ENGINE = create_engine(f"postgresql+pg8000://{USER}:{KEY_VAL}@{HOST}/{DB}", client_encoding='utf8')
+# ENGINE = create_engine(f"postgresql+pg8000://{USER}:{KEY_VAL}@{HOST}/{DB}", client_encoding='utf8')
 
 ENGINE = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
 DB_BASE = classes.db_class
+DB_BASE.metadata.create_all(ENGINE)
 
 
 def session_context_mgr(method):
@@ -32,25 +33,22 @@ def session_context_mgr(method):
     return session_wrapper
 
 
-DB_BASE.metadata.create_all(ENGINE)
-
-
 @session_context_mgr
-def add_instance(session, instances):
-    for instance in instances:
-        session.add(instance)
+def add_object(session, objects):
+    for obj in objects:
+        session.add(obj)
     return
 
 
 @session_context_mgr
-def delete_obj(session, objects):
+def select_object_all(session, query_obj, filter_kwargs):
+    statement = select(query_obj).filter_by(**filter_kwargs)
+    result = session.execute(statement).first()
+    return result
+
+
+@session_context_mgr
+def delete_object(session, objects):
     for obj in objects:
         session.delete(obj)
     return
-
-
-DB_BASE.metadata.create_all(ENGINE)
-user = classes.User('BoogtehWoog', 'BoogtehWoog@gmail.com', 'Boogest', 185, 180)
-
-
-add_instance((user,))
