4e1bc89e74e1f90f5634938843f7df1dc1ef4f6a
Aranda Paige <ArandaPaige@gmail.com>
2021-06-03 20:11:29 -0500

    Addition of weight and lifting history routes.

--- fitness_web_app/userprofile.py
+++ fitness_web_app/userprofile.py
@@ -1,10 +1,36 @@
 from flask import render_template, flash, redirect, url_for
 from fitness_web_app import app, DBASE
-from fitness_web_app.model import User, WeightEntryForm, WeightEntry
+from fitness_web_app.model import User, WeightEntryForm, WeightEntry, SetEntryForm, SetEntry
 from flask_login import login_required, current_user
 
 
 @app.route("/profile/", methods=['GET'])
 def profile():
     form = WeightEntryForm()
+    if form.validate_on_submit():
+        weight_entry = WeightEntry(current_user.id, form.weight.data, form.date.data)
+        DBASE.add(weight_entry)
+        DBASE.commit()
     return render_template('profile.html', title='Profile', form=form)
+
+
+@app.route("/profile/weight_history", methods=['GET'])
+def weight_history():
+    form = WeightEntryForm()
+    if form.validate_on_submit():
+        weight_entry = WeightEntry(current_user.id, form.weight.data, form.date.data)
+        DBASE.add(weight_entry)
+        DBASE.commit()
+    return render_template('profile.html', title='Profile')
+
+
+@app.route("/profile/lifting_history", methods=['GET'])
+def lifting_history():
+    form = SetEntryForm()
+    if form.validate_on_submit():
+        set_entry = SetEntry(
+            current_user.id, form.lift.data, form.weight.data, form.reps.data, form.date.data, form.rpe.data
+        )
+        DBASE.add(set_entry)
+        DBASE.commit()
+    return render_template('profile.html', title='Profile')
