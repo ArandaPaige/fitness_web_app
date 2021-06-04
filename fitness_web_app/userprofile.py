a6e1ad5d56b3aee6fd4d5bf5ffeaa08a787117c4
Aranda Paige <ArandaPaige@gmail.com>
2021-06-03 19:52:38 -0500

    Added weight entry form to profile.

--- fitness_web_app/userprofile.py
+++ fitness_web_app/userprofile.py
@@ -1,9 +1,10 @@
 from flask import render_template, flash, redirect, url_for
 from fitness_web_app import app, DBASE
-from fitness_web_app.model import User
+from fitness_web_app.model import User, WeightEntryForm, WeightEntry
 from flask_login import login_required, current_user
 
 
 @app.route("/profile/", methods=['GET'])
 def profile():
-    return render_template('profile.html', title='Profile')
+    form = WeightEntryForm()
+    return render_template('profile.html', title='Profile', form=form)
