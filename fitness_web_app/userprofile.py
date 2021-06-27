d6fd8514c08f5139eb614526812f0e4e9da9b92d
Aranda Paige <ArandaPaige@gmail.com>
2021-06-27 17:16:05 -0500

    Bootstrap forms added.

--- fitness_web_app/userprofile.py
+++ fitness_web_app/userprofile.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from fitness_web_app import app, DBASE
-from fitness_web_app.model import User, WeightEntryForm, WeightEntry, SetEntryForm, SetEntry
+from fitness_web_app.model import User, WeightEntryForm, DietEntryForm, WeightEntry, SetEntryForm, SetEntry
 from flask_login import login_required, current_user
 
 
@@ -8,15 +8,18 @@ from flask_login import login_required, current_user
 @login_required
 def profile():
     weight_list = WeightEntry.query.filter_by(user_id=current_user.id).all()
-    form = WeightEntryForm()
-    if form.validate_on_submit():
-        if form.date.data is None:
-            weight_entry = WeightEntry(current_user.id, form.weight.data)
+    dietform = DietEntryForm()
+    weform = WeightEntryForm()
+    if dietform.validate_on_submit():
+        pass
+    if weform.validate_on_submit():
+        if weform.date.data is None:
+            weight_entry = WeightEntry(current_user.id, weform.weight.data)
         else:
-            weight_entry = WeightEntry(current_user.id, form.weight.data, form.date.data)
+            weight_entry = WeightEntry(current_user.id, weform.weight.data, weform.date.data)
         DBASE.session.add(weight_entry)
         DBASE.session.commit()
-    return render_template('profile.html', title='Profile', form=form, weight_list=weight_list)
+    return render_template('profile.html', title='Profile', dietform=dietform, weform=weform, weight_list=weight_list)
 
 
 @app.route("/profile/weight_history", methods=['POST', 'GET'])
